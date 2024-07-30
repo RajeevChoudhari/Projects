44406ad01f4efe91d6faf2045d7406faa93f4562
Rajeev Choudhari <rajeevchoudhari007@gmail.com>
2024-07-30 04:03:10 -0400

    I have aids lmao i give up

---
+++ GPTWrapper.py
@@ -0,0 +1,9 @@
+#Project_ID proj_9fPpm9mrCK6gbKWrrT685oEZ
+#Organization_ID org-A15NcSQYP884Nmh5GgsIUH8R
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    organization: "org-A15NcSQYP884Nmh5GgsIUH8R",
+    project: "$proj_9fPpm9mrCK6gbKWrrT685oEZ",
+});
