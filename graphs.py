8854d518c36bf0902b1d0721913a545b078e5ed3
Rajeev Choudhari <Rajeevchoudhari007@gmail.com>
2024-09-04 23:28:42 -0400

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('SPX.csv')
+df = pd.DataFrame(data)
+
+df['Daily Return'] = df['Close'].pct_change() * 100
+df = df.dropna()
+
+sns.set(style='whitegrid')
+plt.figure(figsize=(10, 5))
+sns.scatterplot(x='Volume', y='Daily Return', data=df, edgecolor=None)
+plt.title('Volume vs. Daily Price Change of the S&P 500 Index')
+plt.xlabel('Volume')
+plt.ylabel('% Change')
+
+plt.show()
